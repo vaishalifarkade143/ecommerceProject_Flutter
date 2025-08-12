3cca91144830ea2503026d218f389017c9bf4711
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-12 13:34:58 +0530

    elevated_button_theme is completed TElevatedButtonTheme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -12,6 +12,7 @@ class TAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: T
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
