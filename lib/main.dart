ad466c8959096be6cda4a1c1776c068bf7e3ed44
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-13 17:06:57 +0530

    sizes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerseproject/app.dart';
+import 'package:ecommerseproject/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
+  TColors.linearGradient;
 }
 
 
---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,71 @@
+class TSizes {
+  //padding and margin sizes
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  //Icon sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+  static const double iconXl = 40.0;
+
+  //Font sizes
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+  static const double fontSizeXl = 20.0;
+
+  //Button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonElevation = 4.0;
+
+  //AppBar height
+  static const double appBarHeight = 56.0;
+
+  //Image sizes
+  static const double imageThumbSize = 80.0;
+
+  //Default spacing between sections
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwItems = 16.0;
+  static const double spaceBtwSections = 32.0;
+
+  //Border radius
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0;
+  static const double borderRadiusLg = 12.0;
+
+  //Divider height
+  static const double dividerHeight = 1.0;
+
+  //product item dimentions
+  static const double productImageSize = 120.0;
+  static const double productImageRadious = 16.0;
+  static const double productItemHeight = 160.0;
+
+  //input field sizes
+  static const double inputFieldRadious = 12.0;
+  static const double spaceBtwInputFields = 16.0;
+
+  //Cardsizes
+  static const double cardRadiousLg = 16.0;
+  static const double cardRadiousMd = 12.0;
+  static const double cardRadiousSm = 10.0;
+  static const double cardRadiousXs = 6.0;
+  static const double cardElevation  = 2.0;
+
+  //Image carousel height
+  static const double imageCarouselHeight = 200.0;
+
+  //Loading indicatior size 
+  static const double loadingIndicatorSize = 36.0;
+
+  //Grid view spacing
+  static const double gridViewSpacing = 16.0;
+}
