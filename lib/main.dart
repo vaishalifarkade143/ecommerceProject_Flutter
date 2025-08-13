504c7912ca1ab0e5e4501efb3207bc2a2f9214e4
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-13 13:02:02 +0530

    custom colors added to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
-import 'package:ecommerseproject/utils/theme/theme.dart';
+import 'package:ecommerseproject/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-   const MyApp ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-  }f
