02b5d736b690880c26d2a2b5954ed6bcfd12a620
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-13 17:39:26 +0530

    image_strings.dart done

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+class TImages{
+  //-- App Logo
+  static const String darkAppLogo = 'assets/logos/app_logo.png';
+  static const String lightAppLogo = 'assets/logos/app_logo.png';
+
+  //-- social Logos
+  static const String google = 'assets/logos/onboarding1.png';
+  static const String facebook = 'assets/logos/onboarding1.png';
+
+}
