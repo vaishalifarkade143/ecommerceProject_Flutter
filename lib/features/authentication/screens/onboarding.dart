06384af1e1bf7dcf3be9c0dcb5e228c5130702a4
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-19 13:42:29 +0530

    extract OnBoardingPage widget

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -13,8 +13,22 @@ class OnBoardingScreen extends StatelessWidget {
         body: Stack(
       children: [
         PageView(
-          children: [
-            OnBoardingPage()
+          children: const [
+            OnBoardingPage(
+              image: TImages.onBoardingImage1,
+              title: TTexts.onBoardingTitle1,
+              subtitle: TTexts.onBoardingSubtitle1,
+            ),
+             OnBoardingPage(
+              image: TImages.onBoardingImage2,
+              title: TTexts.onBoardingTitle2,
+              subtitle: TTexts.onBoardingSubtitle2,
+            ),
+             OnBoardingPage(
+              image: TImages.onBoardingImage3,
+              title: TTexts.onBoardingTitle3,
+              subtitle: TTexts.onBoardingSubtitle3,
+            ),
           ],
         )
       ],
@@ -25,30 +39,37 @@ class OnBoardingScreen extends StatelessWidget {
 class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({
     super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
   });
-  
+
+  final String image, title, subtitle;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image(
-          width: THelperFunctions.screenWidth() * 0.8,
-          height: THelperFunctions.screenHeight() * 0.6,
-          image: const AssetImage(TImages.onBoardingImage1),
-        ),
-        Text(
-          TTexts.onBoardingTitle1,
-          style: Theme.of(context).textTheme.headlineMedium,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: TSizes.spaceBtwItems),
-        Text(
-          TTexts.onBoardingSubtitle1,
-          style: Theme.of(context).textTheme.bodyMedium,
-          textAlign: TextAlign.center,
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+            width: THelperFunctions.screenWidth() * 0.8,
+            height: THelperFunctions.screenHeight() * 0.6,
+            image: AssetImage(image),
+          ),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+          Text(
+            subtitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
