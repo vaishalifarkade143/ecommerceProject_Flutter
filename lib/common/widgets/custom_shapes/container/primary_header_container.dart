534e1d26115aadadb526c9a8502f699be0757f03
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-22 12:48:31 +0530

    added widget PrimaryHeaderContainer

---
+++ lib/common/widgets/custom_shapes/container/primary_header_container.dart
@@ -0,0 +1,44 @@
+import 'package:ecommerseproject/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:ecommerseproject/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:ecommerseproject/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryHeaderContainer extends StatelessWidget {
+  const PrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CurvedEdgesWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.only(bottom: 0),
+        child: SizedBox(
+        height: 350,
+        child: Stack(
+          children: [
+            ///Background custom Shape
+            Positioned(
+              top: -150,
+              right: -250,
+              child: CircularContainer(
+                backgroundColor: TColors.textWhite.withOpacity(0.1),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: -300,
+              child: CircularContainer(
+                backgroundColor: TColors.textWhite.withOpacity(0.1),
+              ),
+            ),
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,7 +1,4 @@
-import 'package:ecommerseproject/common/widgets/custom_shapes/container/circular_container.dart';
-import 'package:ecommerseproject/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import 'package:ecommerseproject/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:ecommerseproject/utils/constants/colors.dart';
+import 'package:ecommerseproject/common/widgets/custom_shapes/container/primary_header_container.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,35 +9,8 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
-        children: [
-          CurvedEdgesWidget(
-            child: Container(
-            color: TColors.primary,
-            padding: EdgeInsets.all(0),
-            child: SizedBox(
-            height: 350,
-            child: Stack(
-            children: [
-              Positioned(top: -150,
-              right: -250,
-                child: CircularContainer(
-                  backgroundColor: TColors.textWhite.withOpacity(0.1),
-                ),
-              ),
-              Positioned(top: 100,
-              right: -300,
-                child: CircularContainer(
-                  backgroundColor: TColors.textWhite.withOpacity(0.1),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),,
-          )
-        ],
+        children: [PrimaryHeaderContainer(child: Container())],
       ),
     ));
   }
 }
-
