d9c109e3aebdb88a1c9f9ca253ac669b77e39eb5
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-22 17:02:11 +0530

    THomeAppBar separate widget created inside shop ->screen-> home-> widgets

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,8 +1,6 @@
-import 'package:ecommerseproject/common/widgets/appbar/appbar.dart';
+
 import 'package:ecommerseproject/common/widgets/custom_shapes/container/primary_header_container.dart';
-import 'package:ecommerseproject/common/widgets/products.cart/cart_menu_icon.dart';
-import 'package:ecommerseproject/utils/constants/colors.dart';
-import 'package:ecommerseproject/utils/constants/text_strings.dart';
+import 'package:ecommerseproject/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,29 +16,7 @@ class HomeScreen extends StatelessWidget {
               child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TAppBar(
-                title: Column(children: [
-                  Text(
-                    TTexts.homeAppbarTitle,
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelMedium!
-                        .apply(color: TColors.grey),
-                  ),
-                  Text(
-                    TTexts.homeAppbarSubTitle,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineSmall!
-                        .apply(color: TColors.white),
-                  ),
-                ]),
-                actions: [
-                  CartCounterIcon(
-                    onPressed: () {}, iconColor: TColors.white,
-                  )
-                ],
-              ),
+              THomeAppBar(),
             ],
           ))
         ],
@@ -48,4 +24,5 @@ class HomeScreen extends StatelessWidget {
     ));
   }
 }
+
 
---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerseproject/common/widgets/appbar/appbar.dart';
+import 'package:ecommerseproject/common/widgets/products.cart/cart_menu_icon.dart';
+import 'package:ecommerseproject/utils/constants/colors.dart';
+import 'package:ecommerseproject/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+
+class THomeAppBar extends StatelessWidget {
+  const THomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(children: [
+        Text(
+          TTexts.homeAppbarTitle,
+          style: Theme.of(context)
+              .textTheme
+              .labelMedium!
+              .apply(color: TColors.grey),
+        ),
+        Text(
+          TTexts.homeAppbarSubTitle,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .apply(color: TColors.white),
+        ),
+      ]),
+      actions: [
+        CartCounterIcon(
+          onPressed: () {}, iconColor: TColors.white,
+        )
+      ],
+    );
+  }
+}
