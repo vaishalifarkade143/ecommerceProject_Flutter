335b340af88ad6d395ba193c9cf4c6b51b79c310
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-08-22 16:53:37 +0530

    CartCounterIcon separate widget created

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerseproject/common/widgets/appbar/appbar.dart';
 import 'package:ecommerseproject/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:ecommerseproject/common/widgets/products.cart/cart_menu_icon.dart';
 import 'package:ecommerseproject/utils/constants/colors.dart';
 import 'package:ecommerseproject/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -36,33 +36,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ]),
                 actions: [
-                  Stack(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Iconsax.shopping_bag,
-                          color: TColors.white,
-                        ),
-                      ),
-                      Positioned(
-                        right:0,
-                        child: Container(
-                          width: 18,
-                          height: 18,
-                          decoration: BoxDecoration(
-                            color: TColors.black,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: Center(
-                              child: Text('2',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .apply(color: TColors.white, fontSizeFactor: 0.8))),
-                        ),
-                      )
-                    ],
+                  CartCounterIcon(
+                    onPressed: () {}, iconColor: TColors.white,
                   )
                 ],
               ),
@@ -74,54 +49,3 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-// import 'package:ecommerseproject/common/widgets/appbar/appbar.dart';
-// import 'package:ecommerseproject/common/widgets/custom_shapes/container/primary_header_container.dart';
-// import 'package:ecommerseproject/utils/constants/colors.dart';
-// import 'package:ecommerseproject/utils/constants/text_strings.dart';
-// import 'package:flutter/material.dart';
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             PrimaryHeaderContainer(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   TAppBar(
-//                     title: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           TTexts.homeAppbarTitle,
-//                           style: Theme.of(context)
-//                               .textTheme
-//                               .labelMedium!
-//                               .apply(color: TColors.grey),
-//                         ),
-//                         Text(
-//                           TTexts.homeAppbarSubTitle,
-//                           style: Theme.of(context)
-//                               .textTheme
-//                               .headlineSmall!
-//                               .apply(color: TColors.white),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             // 👇 put the rest of your body here
-//             const SizedBox(height: 100, child: Center(child: Text("Home Content"))),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
