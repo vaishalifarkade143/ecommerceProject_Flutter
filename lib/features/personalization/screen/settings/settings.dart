d58ad18beb531688f10bb02ab873a1dbc495ab69
VaishaliFarkade143 <vaishalifarkade143@gmail.com>
2025-09-17 16:10:49 +0530

    body in SettingScreen  done  created TSettingMenuTile

--- lib/features/personalization/screen/settings/settings.dart
+++ lib/features/personalization/screen/settings/settings.dart
@@ -1,38 +1,137 @@
 import 'package:ecommerseproject/common/widgets/appbar/appbar.dart';
 import 'package:ecommerseproject/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:ecommerseproject/common/widgets/list_tile/settings_menu_tile.dart';
 import 'package:ecommerseproject/common/widgets/list_tile/user_profile_tile.dart';
+import 'package:ecommerseproject/common/widgets/texts/section_heading.dart';
 import 'package:ecommerseproject/utils/constants/colors.dart';
+import 'package:ecommerseproject/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-
             /// -- Header
-          PrimaryHeaderContainer(
-            height: 200,
-            child: Column(
-              children: [
-                ///  AppBar
-               TAppBar(title: Text('Account' , style: Theme.of(context).textTheme.headlineMedium!.apply(color: TColors.white),),),
-              // const SizedBox(height: TSizes.spaceBtwSections,), 
+            PrimaryHeaderContainer(
+              height: 200,
+              child: Column(
+                children: [
+                  ///  AppBar
+                  TAppBar(
+                    title: Text(
+                      'Account',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium!
+                          .apply(color: TColors.white),
+                    ),
+                  ),
+                  // const SizedBox(height: TSizes.spaceBtwSections,),
 
-              ///  UserProfile Card
-              TUserProfileTile(),
-              
+                  ///  UserProfile Card
+                  TUserProfileTile(),
+                ],
+              ),
+            ),
 
-              ],
-            ) )
+            /// --Body
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// -- Account setting
+                  TSectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  SizedBox(height: TSizes.spaceBtwItems),
+
+                  TSettingMenuTile(
+                      icon: Iconsax.safe_home,
+                      title: 'My Addresses',
+                      subTitle: 'Set Shopping delivery address',
+                      onTap: () {}),
+                  TSettingMenuTile(
+                      icon: Iconsax.shopping_cart,
+                      title: 'My Cart',
+                      subTitle: 'Add, remove product and move to checkout',
+                      onTap: () {}),
+                  TSettingMenuTile(
+                      icon: Iconsax.safe_home,
+                      title: 'My Order',
+                      subTitle: 'In-progress and Complete Orders',
+                      onTap: () {}),
+                  TSettingMenuTile(
+                      icon: Iconsax.bank,
+                      title: 'Bank Account',
+                      subTitle: 'Withdraw balance to registerd bank account',
+                      onTap: () {}),
+
+                  TSettingMenuTile(
+                      icon: Iconsax.discount_shape,
+                      title: 'My Coupons',
+                      subTitle: 'List of all the discounted coupons',
+                      onTap: () {}),
+                  TSettingMenuTile(
+                      icon: Iconsax.notification,
+                      title: 'Notifications',
+                      subTitle: 'Set any kind of notification message',
+                      onTap: () {}),
+                  TSettingMenuTile(
+                      icon: Iconsax.security_card,
+                      title: 'Account Privacy',
+                      subTitle: 'Manage data usage and connected account',
+                      onTap: () {}),
+
+                  /// -- App Settings
+                  SizedBox(height: TSizes.spaceBtwSections),
+                  TSectionHeading(
+                    title: 'App Setttings',
+                    showActionButton: false,
+                  ),
+                  SizedBox(height: TSizes.spaceBtwItems),
+                  TSettingMenuTile(
+                      icon: Iconsax.document_upload,
+                      title: 'Load Data',
+                      subTitle: 'Upload Data to your Firebase'),
+                  TSettingMenuTile(
+                    icon: Iconsax.location,
+                    title: 'Geolocation',
+                    subTitle: 'Set reccommendation base on location',
+                    trailing: Switch(value: true, onChanged: (value) {}),
+                  ),
+                    TSettingMenuTile(
+                    icon: Iconsax.security_user,
+                    title: 'Safe Mode',
+                    subTitle: 'Search result is safe for all ages',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+                    TSettingMenuTile(
+                    icon: Iconsax.location,
+                    title: 'HD Image Quality',
+                    subTitle: 'Set image quality to be seen',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+
+                  /// -- Logout Button
+                  const SizedBox(height: TSizes.spaceBtwSections,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(onPressed: (){}, child: const Text('Logout')),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections * 2.5 ,)
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
